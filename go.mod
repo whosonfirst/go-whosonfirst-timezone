a0ea1af43716d8e07d78be9c7ce9fb4dc22c0594
thisisaaronland <devnull@localhost>
2021-10-14 09:43:46 -0700

    block out apply tool, not sure that PIP-ing is what we want to do here

--- go.mod
+++ go.mod
@@ -3,9 +3,15 @@ module github.com/whosonfirst/go-whosonfirst-timezones
 go 1.16
 
 require (
-	github.com/aaronland/go-jsonl v0.0.8
+	github.com/aaronland/go-jsonl v0.0.9
 	github.com/paulmach/orb v0.2.2
+	github.com/whosonfirst/go-whosonfirst-export/v2 v2.4.0
+	github.com/whosonfirst/go-whosonfirst-feature v0.0.15
+	github.com/whosonfirst/go-whosonfirst-iterate/v2 v2.0.1
 	github.com/whosonfirst/go-whosonfirst-spatial v0.3.2
 	github.com/whosonfirst/go-whosonfirst-spatial-sqlite v0.4.1
 	github.com/whosonfirst/go-whosonfirst-spr/v2 v2.0.0
+	github.com/whosonfirst/go-whosonfirst-uri v1.1.0
+	github.com/whosonfirst/go-whosonfirst-writer v0.2.4
+	github.com/whosonfirst/go-writer v0.6.0
 )
---
+++ vendor/github.com/whosonfirst/go-whosonfirst-writer/writer.go
@@ -0,0 +1,55 @@
+package writer
+
+import (
+	"bytes"
+	"context"
+	"errors"
+	"github.com/tidwall/gjson"
+	"github.com/whosonfirst/go-whosonfirst-export/v2"
+	"github.com/whosonfirst/go-whosonfirst-geojson-v2"
+	"github.com/whosonfirst/go-whosonfirst-uri"
+	go_writer "github.com/whosonfirst/go-writer"
+)
+
+func WriteFeature(ctx context.Context, wr go_writer.Writer, f geojson.Feature) error {
+	return WriteFeatureBytes(ctx, wr, f.Bytes())
+}
+
+func WriteFeatureBytes(ctx context.Context, wr go_writer.Writer, body []byte) error {
+
+	ex, err := export.NewExporter(ctx, "whosonfirst://")
+
+	if err != nil {
+		return err
+	}
+
+	ex_body, err := ex.Export(ctx, body)
+
+	if err != nil {
+		return err
+	}
+
+	id_rsp := gjson.GetBytes(ex_body, "properties.wof:id")
+
+	if !id_rsp.Exists() {
+		return errors.New("Missing 'properties.wof:id' property")
+	}
+
+	id := id_rsp.Int()
+
+	rel_path, err := uri.Id2RelPath(id)
+
+	if err != nil {
+		return err
+	}
+
+	br := bytes.NewReader(ex_body)
+
+	_, err = wr.Write(ctx, rel_path, br)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
